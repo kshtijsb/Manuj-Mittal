ca8ea7fcf9088b2a4e7a64682d985c9318a18250
kshitijsb <kshitijshashikant.bhilare2@mail.dcu.ie>
2026-05-14 00:07:23 +0100

    Slim navbar and update logo to full name Manuj Mittal

--- components/header.php
+++ components/header.php
@@ -267,7 +267,7 @@
     <header>
         <div class="container">
             <nav>
-                <a href="index.php" class="logo">M. MITTAL</a>
+                <a href="index.php" class="logo">MANUJ MITTAL</a>
                 <button class="menu-toggle">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                         <line x1="3" y1="12" x2="21" y2="12"></line>
