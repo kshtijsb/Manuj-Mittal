b493fc0b584499a78d80f3241fc5112e45389bf6
kshitijsb <kshitijshashikant.bhilare2@mail.dcu.ie>
2026-06-19 00:12:28 +0100

    chore: initialize composer autoloader dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kshtijsb/manuj-mittal',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '750baf76bf649e0ec4382501ed51b9f7e59a4686',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kshtijsb/manuj-mittal' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '750baf76bf649e0ec4382501ed51b9f7e59a4686',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
